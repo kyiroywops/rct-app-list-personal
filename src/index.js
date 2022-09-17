c8b28d0ef6a0df716d4ab1aff305ad5c87d212b9
Kylian Leroy <aleexisazul@gmail.com>
2022-09-16 23:12:23 -0300

    estructura

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+WebFont.load ({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
