711f80f21fe0eb72f1f634df72ae358ea444d54b
Kylian Leroy <aleexisazul@gmail.com>
2022-09-17 17:38:25 -0300

    registro

--- src/componentes/RegistroUsuarios.js
+++ src/componentes/RegistroUsuarios.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
+import {Header, Titulo, ContenedorBotones, ContenedorHeader} from '../elementos/Header';
 
 const RegistroUsuarios = () => {
     return (  
-        <h1>Registros Usuarios</h1>
+        <>
+         <Helmet>
+                <title>Registro de Usuarios</title>
+         </Helmet>
+         <Header>
+                <ContenedorHeader>
+                    <Titulo>Registro de Usuarios</Titulo>
+                </ContenedorHeader>
+         </Header>
+         
+
+        </>
     );
 }
  
