4f43be2bc31345e32a2ad067e65ca9e23551a265
Kylian Leroy <aleexisazul@gmail.com>
2022-09-17 16:49:07 -0300

    as

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
+import {Header, Titulo, ContenedorHeader, ContenedorBotones} from '../elementos/Header';
 
 const App = () => {
   return ( 
-    <h1>hoaddsdadadsasdla</h1>
-   );
+    <>
+      <Helmet>
+        <title>Administrador de Gastos</title>
+      </Helmet>
+
+      <Header> 
+        <ContenedorHeader>
+          <Titulo>Administrador de Gastos</Titulo>
+          <ContenedorBotones>
+            <button>
+              Categorias
+            </button>
+            <button>
+              Lista de gastos
+            </button>
+          </ContenedorBotones>
+        </ContenedorHeader>
+
+      </Header>
+    </>
+  );
 }
  
 export default App;
