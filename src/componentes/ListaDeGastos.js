3e24a16b6d1bc8083ed36097ea0487f8c5490641
Kylian Leroy <aleexisazul@gmail.com>
2022-09-21 17:11:24 -0300

    context provider hook

--- src/componentes/ListaDeGastos.js
+++ src/componentes/ListaDeGastos.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import {Header, Titulo} from '../elementos/Header';
 import BtnRegresar from '../elementos/BtnRegresar';
-import { useAuth } from '../contextos/AuthContext';
 
 const ListaDeGastos = () => {
-    const {usuario} = useAuth();
+  
    
     return ( 
       <>
