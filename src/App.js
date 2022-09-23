2535b43207ff99f98489c988167e478a9a6ea1c8
Kylian Leroy <aleexisazul@gmail.com>
2022-09-23 19:21:12 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import {Header, Titulo, ContenedorHeader, ContenedorBotones} from './elementos/Header';
 import Boton from './elementos/Boton';
 import BotonCerrarSesion from './elementos/BotonCerrarSesion';
 
 const App = () => {
-  return ( 
-    <>
-      <Helmet>
-        <title>Administrador de Gastos</title>
-      </Helmet>
+  return (
+	<>
+		<Helmet>
+			<title>Agregar Gasto</title>
+		</Helmet>
 
-      <Header> 
-        <ContenedorHeader>
-          <Titulo>Administrador de Gastos</Titulo>
-          <ContenedorBotones>
-            <Boton to="/categorias">Categorias</Boton>
-            <Boton to="/lista">Lista de gastos</Boton>
-            <BotonCerrarSesion/>
-          </ContenedorBotones>
-        </ContenedorHeader>
+		<Header>
+			<ContenedorHeader>
+			<Titulo>Agregar Gasto</Titulo>
+				<ContenedorBotones>
+					<Boton to="/categorias">Categorías</Boton>
+					<Boton to="/lista">Lista de Gastos</Boton>
+					<BotonCerrarSesion />
+				</ContenedorBotones>
+			</ContenedorHeader>
+		</Header>
 
-      </Header>
-    </>
+	
+	</>
   );
 }
  
